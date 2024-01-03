958a6c4fd6768d03b1a586819fbd56d2a74df057
shubham <shubham.singhgkp07@gmail.com>
2024-01-03 16:10:22 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MyrequestComponent } from './myrequest/myrequest.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'navbar', component: NavBarComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'myrequest', component: MyrequestComponent },
+  { path: '**', redirectTo: '/login' } 
+  // Handle any undefined routes by redirecting to login
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
